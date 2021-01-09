927598617a2ec92c3f1ab324257f52b8ac160b87
Biljana <zabiljanupavlovic@gmail.com>
2021-01-09 12:35:16 +0100

    add landing page

---
+++ src/layout/content.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LoginScreen from "../components/loginScreen";
+
+export default function Content() {
+  return (
+    <div>
+      <LoginScreen />
+    </div>
+  );
+}
