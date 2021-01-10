860881db28ed46ba04e10c1fc8216fac2ad36296
Biljana <zabiljanupavlovic@gmail.com>
2021-01-10 17:03:50 +0100

    status and category colors

--- src/components/taskUI/shortTask.jsx
+++ src/components/taskUI/shortTask.jsx
@@ -1,13 +1,35 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function ShortTask({ title, category, status }) {
   return (
     <div className="short-task">
-      <p className="short-task-title">{title}</p>
-      <p>{category}</p>
-      <p>{status}</p>
-      <button className="short-task-btn">Update</button>
-      <button className="short-task-btn">Delete</button>
+      <div className={status}>
+        <div className={category}>
+          <Container>
+            <Row>
+              <Col>
+                {" "}
+                <p className="short-task-title">{title}</p>
+              </Col>
+              <Col>
+                {" "}
+                <p className="short-task-title">{category}</p>
+              </Col>
+              <Col>
+                {" "}
+                <p className="short-task-title">{status}</p>
+              </Col>
+              <Col>
+                <button className="short-task-btn">Update</button>
+                <button className="short-task-btn">Delete</button>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
     </div>
   );
 }
