7b13869873574d608201e19f8fc94027435d97ae
Biljana <zabiljanupavlovic@gmail.com>
2021-01-13 23:44:19 +0100

    search function added

--- src/components/taskUI/taskList.jsx
+++ src/components/taskUI/taskList.jsx
@@ -7,9 +7,10 @@ import Button from 'react-bootstrap/Button';
 //import { Sortable } from "@shopify/draggable";
 
 export default function TaskList(props) {
-  const { onClick } = props;
+  const {onClick} = props
   const [tasks, setTasks] = useState([]);
- 
+  const [search, setSearch] = useState("");
+  const [filteredTasks, setFilteredTasks] = useState([]);
 
   useEffect(() => {
     fetch("/api/users/1/tasks")
@@ -20,6 +21,14 @@ export default function TaskList(props) {
   useEffect(() => {
     console.log(tasks);
   }, [tasks]);
+
+  useEffect(() => {
+    setFilteredTasks(
+      tasks.filter((task) =>
+        task.title.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search, tasks]);
 
   //search function
 
@@ -34,9 +43,7 @@ export default function TaskList(props) {
           id="search"
           placeholder="&#128269; Search"
           className="input-field"
-          onChange={(event) => {
-            setSearchTerm(event.target.value);
-          }}
+          onChange={(e) => setSearch(e.target.value)}
         />
         <select name="status" id="task-area" className="input-select">
           <option value="all">All</option>
@@ -57,6 +64,17 @@ export default function TaskList(props) {
       </form>
       <div className="task-container">
         <Link to="/oneTask">
+
+        {filteredTasks.map((task) => (
+        <ShortTask key={task.id}
+        title={task.title}
+        status={task.status}
+        category={task.category}
+        onClick={(id) => {
+          onClick(task.id);
+        }} {...task} />
+      ))}
+
           {tasks.map((task) => {
               return (
                 <ShortTask
